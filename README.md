72305322a5066e99092fe45816bf2bbd36d45fbe
Juan Pi <jpi@addware.com.ar>
2018-09-11 20:46:05 -0300

    My basic app commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# another-flask-example
-Yet Another Flask Basic Example
+# Yet Another Flask Basic Example
+This is just another Flask Basic example for a demo Purpose
+
+## Getting Started
+
+This simple app just implement Flask framework for a demo example purpose.
+
+
+## Prerequisites
+
+You have to install the following python prerequisites:
+
+```
+$ pip install flask
+$ pip install werkzeug
+```
+
+## Clone the Repo
+
+```
+$ git clone https://github.com/maartinpii/another-flask-example
+```
+## Run the app
+
+```
+$ python app.py
+```
+
+## Testing
+
+You can GET a list of customer that is build inside the app:
+
+```
+From a Postman or a Browser go to: 
+
+http://localhost:5000/customers
+```
+
+You can also POST a new Customer:
+
+```
+From a Postman:
+
+POST METHOD
+form_data:
+id (Key) - Int (Value)
+name (Key) - String (Value)
+
+http://localhost:5000/customers
+```
+
+## Author
+
+* **Juan Pi** :)
+
---
+++ app.py
@@ -0,0 +1,43 @@
+from werkzeug import url_decode
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+
+class MethodRewriteMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        if 'METHOD_OVERRIDE' in environ.get('QUERY_STRING', ''):
+            args = url_decode(environ['QUERY_STRING'])
+            method = args.get('__METHOD_OVERRIDE__')
+            if method:
+                method = method.encode('ascii', 'replace')
+                environ['REQUEST_METHOD'] = method
+        return self.app(environ, start_response)
+
+class Customer(object):
+    def __init__(self, id = None, name = None):
+        self.id = id
+        self.name = name
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'secret'
+app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
+
+@app.route('/customers')
+def list_customers():
+    customers = [ Customer(id=1, name=u'My First Customer'), Customer(id=2, name=u'My Second Customer')]
+    return render_template('list_customers.html', customers=customers)
+
+
+@app.route('/customers', methods=['POST',])
+def create_customer():
+    id = request.form['id']
+    name = request.form['name']
+    customer = Customer(id=id, name=name)
+    flash('Customer %s sucessful saved!' % customer.name)
+    return redirect(url_for('list_customers'))
+
+if __name__ == '__main__':
+    app.run()
